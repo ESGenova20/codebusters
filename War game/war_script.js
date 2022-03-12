4df44eb0d4d0520d252a1d3240eb883072c00941
INMichevska20 <85338764+INMichevska20@users.noreply.github.com>
2022-03-12 15:17:06 +0200

    Add a part of the main function

--- War game/war_script.js
+++ War game/war_script.js
@@ -9,3 +9,24 @@ sprite.src = 'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib
 window.onload = function() {
     spriteExplosion.src = 'https://res.cloudinary.com/dc4stsmlc/image/upload/v1570612478/Codepen/explosion_g9ncyg.png';
 };
+
+//Main function for the whole game
+function game() {
+    var canvas = document.getElementById('canvas'),
+        ctx = canvas.getContext('2d'),
+
+        //Get the height and width for the game shield
+        ctxHeight = ctx.canvas.height = window.innerHeight,
+        ctxWidth = ctx.canvas.width  = window.innerWidth;
+
+    //Set a value of assets
+    var bullets = [],
+        enemies = [],
+        explosions = [],
+        destroyed = 0,
+        record = 0,
+        count = 0,
+        playing = false,
+        gameOver = false,
+        country = {deg: 0}
+}
