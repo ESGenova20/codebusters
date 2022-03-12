c91ab4a90c22041d5a61d2abf7b71908e82bbdd7
INMichevska20 <85338764+INMichevska20@users.noreply.github.com>
2022-03-12 15:08:56 +0200

    Add js file to war folder

---
+++ War game/war_script.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', game);
+
+//Import necessary images
+var sprite = new Image();
+var spriteExplosion = new Image();
+
+sprite.src = 'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80';
+
+window.onload = function() {
+    spriteExplosion.src = 'https://res.cloudinary.com/dc4stsmlc/image/upload/v1570612478/Codepen/explosion_g9ncyg.png';
+};
